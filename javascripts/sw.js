77cb44d6d3c42b7c7f63fec2e42a85f51d079561
anesi <neikhianosime@gmail.com>
2016-07-31 14:29:33 +0100

    Add files via upload

---
+++ javascripts/sw.js
@@ -0,0 +1,25 @@
+/** An empty service worker! */
+
+self.addEventListener('push', function(event) {
+  event.waitUntil(
+    self.registration.showNotification('Got Push?', {
+      body: 'Push Message received'
+   }));
+});
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('the-magic-cache').then(function(cache) {
+    return cache.addAll([
+'/',
+'index.html',
+'javascripts/main.js',
+'stylesheets/stylesheet.css',
+'stylesheets/paginate.css',
+'images/bg_hr.png',
+'site.js';
+]);
+    });
+  );
+});
+
